e121cb4d59ef872a5db05c6acd3ea90644033351
Guilherme-fagundes <guilhermekonflanz94@gmail.com>
2021-01-01 20:01:46 -0300

    add responsive mode

--- cdn/js/dash.js
+++ cdn/js/dash.js
@@ -3,4 +3,28 @@ $(function () {
         $(".dashLeftSiderbarContent ul li.subMenu > ul").slideToggle(500);
     });
 
+    $("#btnMobile").click(function (){
+        $(this).css({display: 'none'});
+        $(".mobileNavSess").css({
+            display: "block",
+            'z-ndex': 10
+        })
+        $(".mobileNavSess .dashLeftSiderbar").css({
+            display: 'block',
+            width: '50%',
+            'z-index': 10,
+            position: 'absolute'
+        })
+
+    });
+
+    $(".mobileNavSess").click(function (){
+        $(this).css({
+            display: 'none'
+        })
+        $('#btnMobile').fadeIn()
+    });
+
+
+
 })
