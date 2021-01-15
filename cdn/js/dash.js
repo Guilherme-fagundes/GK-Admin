b334ecdf6d6402a6eb2b9f95656a152665b29617
Guilherme-fagundes <guilhermekonflanz94@gmail.com>
2021-01-14 22:36:20 -0300

    Correção menu mobile

--- cdn/js/dash.js
+++ cdn/js/dash.js
@@ -3,28 +3,18 @@ $(function () {
         $(".dashLeftSiderbarContent ul li.subMenu > ul").slideToggle(500);
     });
 
-    $("#btnMobile").click(function (){
-        $(this).css({display: 'none'});
-        $(".mobileNavSess").css({
-            display: "block",
-            'z-ndex': 10
-        })
-        $(".mobileNavSess .dashLeftSiderbar").css({
-            display: 'block',
-            width: '50%',
-            'z-index': 10,
-            position: 'absolute'
-        })
+    $("#btnMobile").click(function () {
+        $(".mobileNavSess").fadeIn(500);
 
     });
+    $("body > #mobileSes").click(function (e){
+        // e.preventDefault();
+        var target = e.target.id;
+        if (target == 'mobileSes' || target == 'navItem'){
+            $(this).fadeOut();
+        }
 
-    $(".mobileNavSess").click(function (){
-        $(this).css({
-            display: 'none'
-        })
-        $('#btnMobile').fadeIn()
-    });
-
+    })
 
 
 })
